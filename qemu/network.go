70c60b5d9aa141fe169a5959ac8edf7d7ada12f6
ophum <inagaki0106@gmail.com>
2019-11-19 03:16:01 +0000

    [change] queues=2*vcpus, vectors=2*vcpus+2な感じで作るようにした

--- qemu/network.go
+++ qemu/network.go
@@ -12,24 +12,24 @@ import (
 // TODO:
 //   - すでにアタッチされていた場合、エラー処理を文字列で判定する必要がある
 //   - MACアドレスを変更する
-func (q Qemu) AttachTap(label, tap string, mac net.HardwareAddr, vcpus uint32) error {
+func (q Qemu) AttachTap(label, tap string, mac net.HardwareAddr, queues, vectors uint32) error {
 	netdevID := fmt.Sprintf("netdev-%s", label)
 	devID := fmt.Sprintf("virtio-net-%s", label)
 
 	// check to create netdev
 
-	if err := q.tapNetdevAdd(netdevID, tap, vcpus); err != nil {
+	if err := q.tapNetdevAdd(netdevID, tap, queues); err != nil {
 		return fmt.Errorf("Failed to run netdev_add: err='%s'", err.Error())
 	}
 
-	if err := q.virtioNetPCIAdd(devID, netdevID, mac, vcpus); err != nil {
+	if err := q.virtioNetPCIAdd(devID, netdevID, mac, vectors); err != nil {
 		return fmt.Errorf("Failed to create virtio network device: err='%s'", err.Error())
 	}
 
 	return nil
 }
 
-func (q *Qemu) tapNetdevAdd(id, ifname string, vcpus uint32) error {
+func (q *Qemu) tapNetdevAdd(id, ifname string, queues uint32) error {
 	cmd := struct {
 		ID         string `json:"id"`
 		Type       string `json:"type"`
@@ -45,13 +45,13 @@ func (q *Qemu) tapNetdevAdd(id, ifname string, vcpus uint32) error {
 		true,
 		"no",
 		"no",
-		fmt.Sprintf("%d", vcpus),
+		fmt.Sprintf("%d", queues),
 	}
-
 	bs, err := json.Marshal(map[string]interface{}{
 		"execute":   "netdev_add",
 		"arguments": cmd,
 	})
+
 	if err != nil {
 		return err
 	}
@@ -64,7 +64,7 @@ func (q *Qemu) tapNetdevAdd(id, ifname string, vcpus uint32) error {
 	return err
 }
 
-func (q *Qemu) virtioNetPCIAdd(devID, netdevID string, mac net.HardwareAddr, vcpus uint32) error {
+func (q *Qemu) virtioNetPCIAdd(devID, netdevID string, mac net.HardwareAddr, vectors uint32) error {
 	cmd := struct {
 		Driver  string `json:"driver"`
 		ID      string `json:"id"`
@@ -73,14 +73,22 @@ func (q *Qemu) virtioNetPCIAdd(devID, netdevID string, mac net.HardwareAddr, vcp
 		Mac     string `json:"mac"`
 		Vectors string `json:"vectors"`
 		Mq      string `json:"mq"`
+		Guest_tso4 string `json:"guest_tso4"`
+		Guest_tso6 string `json:"guest_tso4"`
+		Guest_ecn  string `json:"guest_ecn"`
+		Guest_ufo  string `json:"guest_ufo"`
 	}{
 		"virtio-net-pci",
 		devID,
 		netdevID,
 		"pci.0",
 		mac.String(),
-		fmt.Sprintf("%d", 2*vcpus+2),
+		fmt.Sprintf("%d", vectors),
 		"on",
+		"off",
+		"off",
+		"off",
+		"off",
 	}
 
 	bs, err := json.Marshal(map[string]interface{}{
